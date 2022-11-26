0d44439ecfb8d32063f8228771f17a6d9f27282d
Elliot Rancie <erparker00@gmail.com>
2022-11-26 20:34:47 +1100

    mosaic schaff

--- src/components/LandingHeaders.jsx
+++ src/components/LandingHeaders.jsx
@@ -18,11 +18,11 @@ export default function LandingHeaders() {
                     {/* { transform:`scale(.0)` } */}
 
           {/* <h1 className="title-3">&</h1> */}
-          <h1 className="title-1">Out of this World</h1>
-          <h1 className="title-2">Digital Spaces.</h1>
+          <h1 className="title-1">Out of this World Digital Spaces</h1>
+          {/* <h1 className="title-2">Digital Spaces.</h1> */}
           {/* <h1 className="title-4">{zustand}</h1> */}
           {/* <h3 className="title-5">I'm Elliot. </h3> */}
-          <h3 className="title-5">Elliot Rancie is taking web design & develop to the next level and beyond.</h3>
+          {/* <h3 className="title-5">Elliot Rancie is taking web design & develop to the next level and beyond.</h3> */}
           {/* unique user experiences for brands on the web. </h3> */}
           {/* & compelling stories */}
           {/* UI Design
