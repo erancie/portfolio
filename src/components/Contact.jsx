ed81ab626263ee6c03f7807936a952bb79801a7b
Elliot Rancie <erparker00@gmail.com>
2023-03-26 22:10:29 +1100

    contact form schaff, mailchimp added, popup func

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -14,24 +14,24 @@ export default function Contact() {
   const [success, setSuccess] = useState(true)
 
   //put these in effect
-
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
-  //   const result = await addToMailchimp(form.email, {
-  //     FNAME: form.firstName,
-  //     LNAME: form.lastName,
-  //     MESSAGE: form.message
-  //   })
-  //   if(result.result==='success'){
-  //     setSuccess('success')
-  //     setForm({ email: 'email', firstName: 'First Name', lastName: 'Last Name', message: 'message' });
-  //   }
-  //   if(result.result==='error') {
-  //     setSuccess('error') 
-  //     //leave form with details
-  //   }
-  //   console.log(result)
-  // }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setSuccess('error') 
+    // const result = await addToMailchimp(form.email, {
+    //   FNAME: form.firstName,
+    //   LNAME: form.lastName,
+    //   MESSAGE: form.message
+    // })
+    // if(result.result==='success'){
+    //   setSuccess('success')
+    //   setForm({ email: 'email', firstName: 'First Name', lastName: 'Last Name', message: 'message' });
+    // }
+    // if(result.result==='error') {
+    //   setSuccess('error') 
+    //   //leave form with details
+    // }
+    // console.log(result)
+  }
 
   const handleChange = (event)=>{
     const {name, value} = event.target
@@ -62,29 +62,33 @@ export default function Contact() {
         setSuccess(null)
       }
     }
-    document.addEventListener("mousedown", handleOutsideClick);
+    useEffect(()=>{
+      document.addEventListener("mousedown", handleOutsideClick);
+    }, [])
       // useEffect(()=>{
   //   return document.removeEventListener("mousedown", handleOutsideClick)
   // }, [ref])
   
     if(success==='success') {
       return (
-        // <div className="popup-container" 
-        //   >
+        <div className="popup-container" 
+          >
           <div className='form-success' ref={ref} onClick={handleOutsideClick} >
             <p>Thanks for reaching out!</p>
             <span className='dismiss' onClick={(e)=>setSuccess(null)}
             >X</span>
           </div>
-        // </div>
+         </div>
       )
     }
     if(success==='error'){
       return (
-        <div className='form-fail' ref={ref} onClick={handleOutsideClick}>
-          <p>Please try sending again</p>
-          <span className='dismiss' onClick={(e)=>setSuccess(null)}
-            >X</span>
+        <div className="popup-container" >
+          <div className='form-fail' ref={ref} onClick={handleOutsideClick}>
+            <p>Please try sending again</p>
+            <span className='dismiss' onClick={(e)=>setSuccess(null)}
+              >X</span>
+          </div>
         </div>
       )
     }
@@ -98,7 +102,7 @@ export default function Contact() {
       {FormSent()}
       {/* <div className='form-success'>Thanks for reaching out!</div> */}
 
-      <form className="contact-form" onSubmit={handleSubmit}>
+      <form className="contact-form" onSubmit={handleSubmit} >
 
         <label>
           First Name
