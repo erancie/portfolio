e3bde4b588319db0966fc2a8b2e4695af39928c4
Elliot Rancie <erparker00@gmail.com>
2023-04-30 15:56:09 +1000

    styles bug fix

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -17,6 +17,7 @@ export default function Home() {
         <Layout >
           <BackgroundScene />
           <LandingHeaders />
+          {/* introduction here  */}
           <ProjectsList /> 
           <Contact />
         </Layout> 
