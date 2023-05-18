a1a6232b048a7f608382e1b1e057878afe43bc52
Elliot Rancie <erparker00@gmail.com>
2023-05-18 19:00:07 +1000

    mc bug fixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
       resolve: 'gatsby-plugin-mailchimp', // see: https://www.gatsbyjs.com/plugins/gatsby-plugin-mailchimp/
       options: {
           endpoint: process.env.GATSBY_MAILCHIMP_ENDPOINT,
-          timeout: 3500,
+          timeout: 10000,
       },
     },
   ],
