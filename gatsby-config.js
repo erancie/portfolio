8461c795772fcca9f856fe64ff8fe69dae6d8f4c
Elliot Rancie <erparker00@gmail.com>
2023-08-28 20:37:03 +1000

    google analytics implemented

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId:  process.env.GA_TRACKING_ID,
+        trackingIds: [ process.env.GA_TRACKING_ID ]
       },
     },
   ],
